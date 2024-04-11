b2bdb76fe961d9030aae6664455d1e24e80bc1aa
handy <handyplus@163.com>
2024-04-11 14:55:19 +0800

    移除谷歌Google Funding Choices 2

--- docs/.vitepress/theme/head.ts
+++ docs/.vitepress/theme/head.ts
@@ -10,11 +10,6 @@ export const head: HeadConfig[] = [
   ['meta', { name: 'referrer', content: 'no-referrer' }],
   ['script', { src: '/_vercel/insights/script.js' }],
   [
-    'script',
-    {},
-    `(function() {function signalGooglefcPresent() {if (!window.frames['googlefcPresent']) {if (document.body) {const iframe = document.createElement('iframe'); iframe.style = 'width: 0; height: 0; border: none; z-index: -1000; left: -1000px; top: -1000px;'; iframe.style.display = 'none'; iframe.name = 'googlefcPresent'; document.body.appendChild(iframe);} else {setTimeout(signalGooglefcPresent, 0);}}}signalGooglefcPresent();})();`
-  ],
-  [
     "link",
     { rel: "stylesheet", href: "/static/css/fancybox.css" },   //全局控制图片放大样式
   ],
