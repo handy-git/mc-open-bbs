63c569ce431230cb5ffb3ec40303cd2849fb743d
handy <handyplus@163.com>
2024-04-11 14:56:34 +0800

    添加keywords

--- docs/.vitepress/theme/head.ts
+++ docs/.vitepress/theme/head.ts
@@ -1,17 +1,20 @@
-import type { HeadConfig } from 'vitepress';
+import type { HeadConfig } from 'vitepress'
 
-// console.log('dddddd',frontmatter.value)
 export const head: HeadConfig[] = [
-  ['link', { rel: 'icon', href: '/favicon.ico' }],
-  ['link', { rel: 'icon', href: '/logo.png' }],
-  ['link', { rel: 'apple-touch-icon', sizes: '180x180', href: '/logo.png' }], //全局控制图片放大样式
-  ['meta', { name: 'viewport', content: 'width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no' }],
-  ['meta', { property: 'og:image', content: '/logo.png' }],
-  ['meta', { name: 'referrer', content: 'no-referrer' }],
-  ['script', { src: '/_vercel/insights/script.js' }],
-  [
-    "link",
-    { rel: "stylesheet", href: "/static/css/fancybox.css" },   //全局控制图片放大样式
-  ],
-  ["script", { src: "/static/js/fancybox.umd.js" }],  //全局控制图片放大交互
-];
+    ['link', { rel: 'icon', href: '/favicon.ico' }],
+    ['link', { rel: 'icon', href: '/logo.png' }],
+    ['link', { rel: 'apple-touch-icon', sizes: '180x180', href: '/logo.png' }], //全局控制图片放大样式
+    ['meta', {
+        name: 'viewport',
+        content: 'width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no'
+    }],
+    ['meta', {
+        name: 'keywords',
+        content: 'MCBBS,我的世界,Minecraft'
+    }],
+    ['meta', { property: 'og:image', content: '/logo.png' }],
+    ['meta', { name: 'referrer', content: 'no-referrer' }],
+    ['script', { src: '/_vercel/insights/script.js' }],
+    ['link', { rel: 'stylesheet', href: '/static/css/fancybox.css' }], //全局控制图片放大样式
+    ['script', { src: '/static/js/fancybox.umd.js' }]  //全局控制图片放大交互
+]
