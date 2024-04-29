b77106fc462eeb98f725830e5a73bfaab30eaa59
handy <handyplus@163.com>
2024-04-29 11:14:18 +0800

    添加扫码领红包和 push移除day

--- docs/.vitepress/theme/components/Push.vue
+++ docs/.vitepress/theme/components/Push.vue
@@ -18,10 +18,9 @@ import { data as themeposts } from '../posts.data'
 const data = computed(() => initArchives(themeposts))
 const pushUrl = ()=>{
   const date = new Date();
-  var year = date.getFullYear();  
-  var month = (1 + date.getMonth()).toString().padStart(2, '0');  
-  var day = date.getDate().toString().padStart(2, '0');  
-  return `https://github.com/handy-git/mc-open-bbs/new/main/docs/posts/${year}/${month}/${day}`;
+  var year = date.getFullYear();
+  var month = (1 + date.getMonth()).toString().padStart(2, '0');
+  return `https://github.com/handy-git/mc-open-bbs/new/main/docs/posts/${year}/${month}`;
 }
 </script>
 
